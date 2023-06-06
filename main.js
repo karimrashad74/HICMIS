2280f8ae39e4922bd28bdd07cb832d69068bda60
karimrashad74 <89028559+karimrashad74@users.noreply.github.com>
2023-06-06 22:14:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var typed = new Typed('.anim', {
+    // Waits 1000ms after typing "First"
+    strings: [
+      'To HICMIS',
+      ],
+      typeSpeed: 90,
+      backSpeed: 100,
+      loop:true,
+     
+  
+  });
+
+ const dark = document.getElementById("dark")
+ const body = document.getElementById("body")
+
+ dark.addEventListener("click", (eo) => {
+
+  body.classList.toggle("dark")
+  // array 
+
+
+  
+
+
+
+
+
+
+})
